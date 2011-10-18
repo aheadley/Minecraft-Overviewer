37a6815699564f643454a4dd9ea61c33775e12af
Alex Headley <aheadley@waysaboutstuff.com>
2011-10-18 09:06:02 -0400

    more work on world/regionset

--- overviewer_core/regionset.py
+++ overviewer_core/regionset.py
@@ -1,31 +1,109 @@
-def base36encode(number, alphabet='0123456789abcdefghijklmnopqrstuvwxyz'):
-    """Convert an integer to a base36 string.
+#    This file is part of the Minecraft Overviewer.
+#
+#    Minecraft Overviewer is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or (at
+#    your option) any later version.
+#
+#    Minecraft Overviewer is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+#    Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with the Overviewer.  If not, see <http://www.gnu.org/licenses/>.
+
+import os.path
+import cPickle
+import nbt
+import glob
+
+"""
+Notes:
+path = non-absolute path to a file or dir, may or may not include a directory part
+filename = only the basename/filename, should only be used for files
+dirname = only the dirname/name of the directory, does not include parent directory
+Xfile = object representing a file
+Xfilename = corresponding filename for X
+Xpath = corresponding path for X
+"""
+
+class World(object):
+    """Represents a collection of dimensions (region sets)
     """
-    if not isinstance(number, (int, long)):
-        raise TypeError('Number must be an integer: %s' % repr(number))
-    
-    new_number = abs(number)
-    base36 = ''
-    # Special case for zero
-    if number == 0:
-        base36 = '0'
-    else:
-        while new_number != 0:
-            new_number, i = divmod(new_number, len(alphabet))
-            base36 = alphabet[i] + base36
 
-        if number < 0:
-            base36 = "-" + base36
-    return base36
+    #TODO dict-like region set access?
 
+    LEVEL_FILE          = 'level.dat'
+    BIOME_FILENAME_TPL  = 'b.%d.%d.biome'
+    BIOME_DIR_NAME      = 'biomes'
+
+    def __init__(self, path, **kwargs):
+        self.path = path
+        self._region_sets = dict(map(
+            lambda region_set: return (region_set.name, region_set),
+            self._get_region_sets()))
+
+    def get_region_set(self, name):
+        """Get a RegionSet object by region name.
+        """
+        #TODO this is completely wrong, region sets won't have names because
+        # they don't have level.dats, unless we use the directory name as their
+        # name
+        return self._region_sets[name]
+
+    def general_biome_data_available(self):
+        """Check if any biome data is available at all
+        """
+        return len(glob.glob(os.path.join(self.world.path, self.BIOME_DIR_NAME,
+            self.BIOME_FILENAME_TPL.replace('%d', '*'))) > 0
+
+    def region_biome_data_available(self, regionX, regionY):
+        """Check if biome data exists for a specific region
+        """
+        return os.path.exists(self._get_biome_data_path(regionX, regionY))
+
+    def _get_biome_data_path(self, regionX, regionY):
+        """Get the path for a biome data file
+        """
+        return os.path.join(self.world.path, self.BIOME_DIR_NAME,
+            self.BIOME_FILENAME_TPL % (regionX, regionY))
+
+    def get_biome_data(self, chunkX, chunkY):
+        """
+        """
+        pass
+
+    def _get_region_set_paths(self):
+        """Get a list of dirs in this worlds path for dirs with region files
+        in them
+        """
+        #TODO cache this? also, it's probably horribly inefficient
+        # find all region files, run dirname on them, remove dupes
+        #probably faster to walk path manually
+        return list(set(map(os.path.dirname, os.path.join(
+            self.path, '*', RegionSet.REGION_FILENAME_TPL.replace('%d', '*')))))
+
+    def _get_region_sets(self):
+        """Get a list of RegionSet objects for every region set path in
+        _get_region_set_paths()
+        """
+        region_sets = []
+        for path in self._get_region_set_paths():
+            region_sets.append(RegionSet(self,
+                path, self._config.get('region_set_config', {})))
+        return region_sets
+            
 class RegionSet(object):
-    """Represents a set of region files that make up a world/dimension
+    """Represents the set of region files that make up a single dimension in a
+    world.
     """
     
-    SETTINGS_FILE   = 'overviewer.dat'
+    SETTINGS_FILE       = 'overviewer.dat'
     #these should probably be filename templates
-    BIOME_FILE_EXT  = 'biome'
-    REGION_FILE_EXT = 'mcr'
+    REGION_FILENAME_TPL = 'r.%d.%d.mcr'
+
+    DEFAULT_DATA        = {}
     
     REGION_FILE_LIMIT   = 256
     #TODO i don't even know what these are used for
@@ -34,39 +112,82 @@ class RegionSet(object):
     def __init__(self, world, path, **kwargs):
         self.world = world
         self.name = self._get_name(path)
-        self.path = os.join(self.world.path, path)
-        self._config = kwargs
-        try:
-            self._load_settings()
-        except: #TODO: what goes here? file not found?
-            #print log message
+        if self.world.path[:len(path)] == path:
+            self.path = path
         else:
-            #TODO: this needs a conditional on force render
-            self._config = kwargs
+            #the world path doesn't contain the region set path, this should
+            #  probably never happen
+            #TODO pick better exception
+            raise Exception("RegionSet.path ({0}) not in World.path ({1})".format(
+                path, self.world))
+        self.path = os.join(self.world.path, path)
+        self._persistent_data = self.DEFAULT_DATA
+        try:
+            self._persistent_data.update(self._load_settings())
+        except: #TODO: what goes here? file not found?
+            logging.debug("Failed to load region set settings")
+        self._persistent_data.update(kwargs)
+        self.bounds = self._find_bounds()
             
     def _get_name(self, path):
         """Get the actual name of a world from the level.dat file
         """
-        #read name from level.dat
-        return 'world'
+        levelnbt = nbt.load(self.get_level_path())
+        try:
+            name = levelnbt[0]['Data']['LevelName']
+        except (IndexError, KeyError):
+            #assume levelname is missing
+            name = "world"
+        return name
+
+    def _get_pickle_filename(self):
+        """
+        """
+        #TODO this should actually pull from the output dir but we don't know
+        #  about it yet.
+        return os.path.join(self.path, self.SETTINGS_FILE)
         
-    def _load_settings(self):
-        #read data from pickle file into self._config
+    def _read_settings(self):
+        #read data from pickle file into self._persistent_data
         #TODO: merge or overwrite?
-        return
+        settings = {}
+        pickle_filename = self._get_pickle_filename()
+        if os.path.exists(pickle_filename):
+            with open(pickle_filename, 'rb') as pickle_file:
+                settings = cPickle.load(pickle_file)
+        return settings
         
-    def save_settings(self):
-        #write self._config to pickle file
-        return
+    def write_settings(self):
+        #write self._persistent_data to pickle file
+        pass
     
     def biome_data_available(self):
-        return len(glob.glob(os.join(self.path,'*.{0}' % self.BIOME_FILE_EXT))) > 0
+        """Check if any biome data is available
+        """
+        return len(glob.glob(os.join(self.world.path, self.BIOME_DIR_NAME,
+            self.BIOME_FILENAME_TPL.replace('%d', '*'))) > 0
+
+    def _get_biome_data_path(self, regionX, regionY):
+        """
+        """
+        return os.path.join(self.world.path, self.BIOME_DIR_NAME,
+            self.BIOME_FILENAME_TPL % (regionX, regionY))
+
+    def get_biome_data(self, chunkX, chunkY):
+        """
+        """
+        pass
+
+    def get_region_paths(self):
+        return glob.glob(os.path.join(
+            self.path, self.REGION_FILENAME_TPL.replace('%d', '*'))
     
     def get_region_path(self, chunkX, chunkY):
         """
         """
         return os.join(self.path,
-            'r.%i.%i.%s' % (chunkX//32, chunkY//32, self.REGION_FILE_EXT))
+            self.REGION_FILENAME_TPL %
+                (chunkX//32, chunkY//32, self.REGION_FILE_EXT))
     
     def get_chunk(self, chunkX, chunkY):
         """
@@ -98,6 +219,19 @@ class RegionSet(object):
         """
         """
         #TODO this probably replaces World.go which is a stupid name
+        #also, figure out how to handle regionlist
+        minX = maxX = minY = maxY = 0
+        for regionX, regionY in self._region_file_iterator():
+            minX = min(minX, regionX)
+            maxX = max(maxX, regionX)
+            minY = min(minY, regionY)
+            maxY = max(maxY, regionY)
+        if sum(map(abs, [minX, maxX, minY, maxY])) is 0:
+            logging.error("No regions found")
+            #TODO handle this better
+            raise Exception("exit 1")
+        else:
+            
     
     def get_id_hash(self):
         """Get a hash representing a set config parameters. If this doesn't
@@ -105,10 +239,27 @@ class RegionSet(object):
         force render so people don't get maps with a grabbag set of tiles.
         """
         #TODO: this is almost guaranteed to be wrong, fix it
-        return mhash.sha1(repr(self._config))
+        #is probably also not needed
+        return mhash.sha1(repr(self._persistent_data))
         
-    def _get_region_file_iterator(self):
+    def _region_file_iterator(self, region_paths=None):
+        """Iterates over region filenames in the set returning their coords
         """
-        """
-        #TODO replaces World._iterate_regionfiles
-        yield (0,0)
+        #TODO add some validation
+        temp = 0
+        for region_filename in map(os.path.basename,
+                self.get_region_paths() if region_paths is None else region_paths)
+            regionX, regionY = map(int, region_filename.split('.')[1:3])
+            #TODO refactor this into a north_transform method
+            if self._config['north_directon'] == 'upper-left':
+                temp = regionX
+                regionX = -regionY - 1
+                regionY = temp
+            elif self._config['north_directon'] == 'upper-right':
+                regionX = -regionX - 1
+                regionY = -regionY - 1
+            elif self._config['north_directon'] == 'lower-right':
+                temp = regionX
+                regionX = regionY
+                regionY = -temp - 1
+            yield (regionX, regionY)
